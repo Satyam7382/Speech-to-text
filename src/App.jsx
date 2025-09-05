8b44b71881f18329603c4e113f4b604e78ec7feb
Satyam7382 <satyamsinghdbg2@gmail.com>
2025-09-05 13:52:43 +0530

    Speech-to-Text

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Speech to Text Project 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
